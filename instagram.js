ae1d66674ee07aa255128c955b7fba5402df481c
John Ciacia <john.ciacia@gmail.com>
2014-06-29 00:44:21 -0400

    refactor

--- instagram.js
+++ instagram.js
@@ -1,65 +1,113 @@
-var Instagram = {
-  
-  call: function(data) {
-    var https = require('https');
-    var options = {
-      hostname: 'instagram.com',
-      port: 443,
-      path: '/api/v1/accounts/login/',
-      method: 'POST',
-      headers: {
-        'Accept': '*/*',
-        'User-Agent': 'Instagram 4.0.2 Android (10/3.2.2; 240dpi; 600x1024; samsung; SGH-I896; SGH-I896; smdkc210; en_GB)',
-        'Accept-Language': 'en-us,en;q=0.5',
-        'Accept-Charset': 'ISO-8859-1,utf-8',
-        'Connection': 'keep-alive',
-      }
+var instagram = function(key, uuid) {
+
+    var Q = require('q'),
+        responseBody = '',
+        cookies = [];
+
+    if(!key || !uuid) {
+        throw {
+            name: 'TypeError',
+            message: 'Key and UUID must be set'
+        }
+    }
+
+    /**
+     *
+     */
+    var request = function(method, resource, data, signed) {
+        var signed = typeof signed === 'undefined' || signed,
+            transport = signed ? require('https') : require('http'),
+            port = signed ? 443 : 80,
+            options, 
+            req,
+            deferred = Q.defer();
+
+        options = {
+            hostname: 'instagram.com',
+            port: port,
+            path: '/api/v1/' + resource,
+            method: method,
+            headers: {
+                'Accept': '*/*',
+                'User-Agent': 'Instagram 4.0.2 Android (10/3.2.2; 240dpi; 600x1024; samsung; SGH-I896; SGH-I896; smdkc210; en_GB)',
+                'Accept-Language': 'en-us,en;q=0.5',
+                'Accept-Charset': 'ISO-8859-1,utf-8',
+                'Connection': 'keep-alive',
+                'Cookie': cookies.join(', ')
+            }
+        };
+
+        req = transport.request(options, function(res) {
+            var responseBody = '';
+            res.setEncoding('utf8');
+            cookies = res.headers['set-cookie'];
+            
+            res.on('data', function(chunk) {
+                responseBody += chunk;
+            });
+
+            res.on('end', function() {
+                console.log(responseBody);
+                deferred.resolve(responseBody);
+            });
+        });
+
+        'get' === method.toLowerCase() || req.write(getSignedRequest(data));
+        req.end();
+
+        req.on('error', function(e) {
+            deferred.reject(e.message)
+        });
+
+        return deferred.promise
+    };
+
+    /**
+     *
+     */
+    var getSignedRequest = function(data) {
+        var data = JSON.stringify(data),
+            post = {
+                'ig_sig_key_version': '4',
+                'signed_body': getSignature(data) + '.' + data
+            };
+
+        return require('querystring').stringify(post);
+    };
+
+    /**
+     *
+     */
+    var getSignature = function(msg) {
+        return require('crypto').createHmac('SHA256', key).update(msg).digest('hex');
     };
 
 
-    var req = https.request(options, function(res) {
-      res.on('data', function(d) {
-        process.stdout.write(d);
-      });
-    });
+    return {
+        login: function(username, password) {
+            return request('post', 'accounts/login/', {
+                username: username,
+                password: password,
+                guid: uuid,
+                device_id: 'android-' + uuid
+            });
+        },
 
-    req.write(this.signed_request(data));
-    req.end();
+        suggested: function() {
+            return request('get', 'friendships/suggested/');
+        },
 
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-    });
-
-
-  },
-
-  signed_request: function(data) {
-    var querystring = require("querystring");
-    var data = JSON.stringify(data);
-
-    var post = {
-      'ig_sig_key_version': '4',
-      'signed_body': this.signature(data) + '.' + data
+        popular: function() {
+            return request('get', 'feed/popular/');
+        } 
     }
-    return querystring.stringify(post);
-  },
+}
 
-  signature: function(msg) {
-    return require('crypto').createHmac('SHA256', this.key).update(msg).digest('hex');
-  },
+var key = '',
+    uuid = '',
+    ig = instagram(key, uuid);
 
-  login: function(username, password) {
-    this.call({
-      username: username,
-      password: password,
-      guid: this.uuid,
-      device_id: 'android-' + this.uuid
-    });
-  }
-
-} 
-
-Instagram.key = '';
-Instagram.uuid = '';
-Instagram.login('username', 'password');
+ig.login('', '').then(function() {
+    ig.popular();
+});
 
