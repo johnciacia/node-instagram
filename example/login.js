2bf28cfd3bbd5df4aff051fbd8345a9334c1071c
John Ciacia <john.ciacia@gmail.com>
2014-07-04 10:04:07 -0400

    add example

---
+++ example/login.js
@@ -0,0 +1,9 @@
+var uuid = require('uuid').v4(),
+    key = '',
+    ig = instagram(key, uuid);
+
+ig.login('username', 'password').then(function() {
+    ig.popular().then(function(response) {
+      console.log(response);
+    });
+});
